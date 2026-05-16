013ce837bc86a450597b9701d88a4cd4380c8d6a
MrFolium <MrVentus@proton.me>
2026-05-16 18:53:55 +0500

    garbage was deleted

--- java_backup/com/mrfolium/willowicons/services/ClockWidget.java
+++
@@ -1,5 +0,0 @@
-package com.mrfolium.willowicons.services;
-
-import candybar.lib.services.CandyBarWidgetService;
-
-public class ClockWidget extends CandyBarWidgetService {}
--- java_backup/com/mrfolium/willowicons/services/MuzeiService.java
+++
@@ -1,5 +0,0 @@
-package com.mrfolium.willowicons.services;
-
-import candybar.lib.services.CandyBarMuzeiService;
-
-public class MuzeiService extends CandyBarMuzeiService {}
